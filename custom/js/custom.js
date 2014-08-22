68df7bb987706cad60fa800b9fa90e9a95732bdf
Jobin Jose <jobinjose@outlook.com>
2014-08-22 18:29:36 +0530

    random

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -3,6 +3,54 @@ function scrollToAnchor(aid){
     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
 }
 
+$("#top").click(function() {
+   scrollToAnchor('start');
+});
+
+$("#portfolio").click(function() {
+   scrollToAnchor('port');
+});
+
 $("#contact").click(function() {
    scrollToAnchor('cont');
 });
+
+
+$(document).ready(function () {
+
+    var menu = $('.menu');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('.menu').addClass('sticky');
+            $('.content').addClass('menu-padding');
+        } else {
+            $('.menu').removeClass('sticky');
+            $('.content').removeClass('menu-padding');
+        }
+
+
+    }
+
+    document.onscroll = scroll;
+
+
+	$('.fancybox-thumbs').fancybox({
+		prevEffect : 'none',
+		nextEffect : 'none',
+
+		closeBtn  : false,
+		arrows    : false,
+		nextClick : true,
+
+		helpers : {
+			thumbs : {
+				width  : 50,
+				height : 50
+			}
+		}
+	});
+
+});
+
